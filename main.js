508dc7c9c312c8d9cfcc1766ad0481b675602491
mozdalif <mozsocia@gmail.com>
2023-05-08 21:04:00 +0600

    complete

---
+++ main.js
@@ -0,0 +1,18 @@
+// get references to the input elements and the button
+const weightInput = document.getElementById('weightInput');
+const heightInput = document.getElementById('heightInput');
+const calculateBtn = document.getElementById('calculateBtn');
+const result = document.getElementById('result');
+console.log("jjj");
+// add an event listener to the button
+calculateBtn.addEventListener('click', (e) => {
+  // get the values of the input fields and convert them to numbers
+  const weight = parseFloat(weightInput.value);
+  const height = parseFloat(heightInput.value);
+
+  // calculate the BMI
+  const bmi = weight / (height * height);
+  console.log("hh",bmi);
+  // display the result
+  result.innerHTML = `Your BMI is: ${bmi.toFixed(2)}`;
+});
